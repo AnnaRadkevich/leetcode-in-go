107181b770cacdf96a21c7ca9463d2bef63749f7
AnnaRadkevich <annaradkevic58@gmail.com>
2023-03-25 17:46:11 +0300

    Remove duplicates

---
+++ removeduplicates/remove_duplicates_test.go
@@ -0,0 +1,33 @@
+package leetcodeInGo
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRemoveDuplicates(t *testing.T) {
+	tests := []struct {
+		name string
+		got  []int
+		want []int
+	}{
+		{
+			name: "no duplicates",
+			got:  []int{1, 2, 3},
+			want: []int{1, 2, 3},
+		},
+		{
+			name: "duplicates",
+			got:  []int{1, 2, 2, 3, 3, 4},
+			want: []int{1, 2, 3, 4},
+		},
+	}
+	for _, test := range tests {
+		l := removeDuplicates(test.got)
+
+		require.Equal(t, len(test.want), l)
+		require.Equal(t, test.want, test.got[:l])
+	}
+
+}
